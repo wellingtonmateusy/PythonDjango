6288c06200ee286ad03f0cfbe5190d5a14d60c82
wellingtonmateusy <wellington.mateussenac@gmail.com>
2023-12-20 20:45:07 -0300

    Update

--- Quiz/Qsm.py
+++ Quiz/Qsm.py
@@ -12,30 +12,29 @@ class Qsm:
         self.opcao = self.radio_buttons()
         self.mostrar_opcoes()
         self.buttons()
-        self.tam_dados = len(pergunta)
+        self.tam_dados = len(questao)
         self.correto = 0
-        
 
     def mostrar_resultado(self):
-        
         cont_err = self.tam_dados - self.correto
         correto = f'Acertos: {self.correto}'
         erros = f'Erros: {cont_err}'
 
-        score = int(self.correto/ self.tam_dados * 100)
-        
+        score = int(self.correto / self.tam_dados * 100)
+
         resultado = f'Placar: {score}'
 
         mb.showinfo('Resultado', f'{resultado}\n{correto}\n{erros}')
-        
+    
     def ver_pergunta(self, pergunta_no) -> bool:
         
         if self.op_escolhida.get() == resposta[pergunta_no]:
             return True
-        
+
     def btnProx(self):
         if self.ver_pergunta(self.pergunta_no):
             self.correto += 1
+
         self.pergunta_no += 1
 
         if self.pergunta_no == self.tam_dados:
@@ -44,23 +43,34 @@ class Qsm:
         else:
             self.mostra_pergunta()
             self.mostrar_opcoes()
-            
+    
     def buttons(self):
-        btnProximo = Button(janela, text = 'Próxima', command = self.btnProx,width = 10, bg = 'black', fg = 'white',font = ('arial, 16, bold'))
+        btnProximo = Button(janela, 
+                            text = 'Próxima', 
+                            command = self.btnProx,
+                            width = 10, bg = 'black', fg = 'white',
+                            font = ('arial', 16, 'bold'))
         btnProximo.place(x = 350, y = 350)
 
-        btnSair = Button(janela, text = 'Sair', command = janela.destroy, width = 10, bg = 'black', fg = 'white',font = ('arial, 16, bold'))
+        btnSair = Button(janela, 
+                        text = 'Sair', 
+                        command = janela.destroy,
+                        width = 10, 
+                        bg = 'black', 
+                        fg = 'white',
+                        font = ('arial', 16, 'bold'))
+        
         btnSair.place(x = 700, y = 50)
-    
+
     def mostrar_opcoes(self):
         val = 0
         
-        self.opt_selected.set(0)
+        self.op_escolhida.set(0)
 
         for op in opcoes[self.pergunta_no]:
             self.opcao[val]['text'] = op
             val += 1
-    
+
     def mostra_pergunta(self):
         pergunta_no = Label(janela, text = questao[self.pergunta_no], width = 60,
         font = ('arial' ,16, 'bold' ), anchor = 'w' )
@@ -69,16 +79,16 @@ class Qsm:
     def mostra_titulo(self):
         titulo = Label(janela, text= 'Quiz', width = 50, bg = "green",fg = 'white', font = ('arial', 20, 'bold'))
         titulo.place(x = 0, y = 2)
-    
+
     def radio_buttons(self):
         
         q_list = []
         
         y_pos = 150
         
-        while len(q_list) < 4:
+        while len(q_list) < 5:
             
-            radio_btn = Radiobutton(janela, text = ' ',variable = self.opt_selected,
+            radio_btn = Radiobutton(janela, text = ' ',variable = self.op_escolhida,
             value = len(q_list) + 1,font = ('arial', 14))
             
             q_list.append(radio_btn)
@@ -88,8 +98,9 @@ class Qsm:
     
         return q_list
     
+
 janela = Tk()
-janela.geometry('800x400')
+janela.geometry('800x450')
 janela.title('SENAC - Quiz')
 
 with open('./Quiz/dados.json') as f:
@@ -99,6 +110,6 @@ questao = (dado['perguntas'])
 opcoes = (dado['opcoes'])
 resposta = (dado['resposta'])
 
-janela = Qsm
+qsm = Qsm()
 
-janela.mainloop(Qsm)
+janela.mainloop()
