72bb2a1d993900ec420845dea49920bfc700129e
wellingtonmateusy <wellington.mateussenac@gmail.com>
2024-01-11 22:31:34 -0300

    Update ScriptExerc_Livro.sql

--- SQL_exemplos/ScriptExerc_Livro.sql
+++ SQL_exemplos/ScriptExerc_Livro.sql
@@ -10,13 +10,46 @@ CREATE TABLE livro(
     preco_livro DOUBLE
 );
 
-INSERT INTO LIVROS VALUES('12345678901', 'Cavaleiro Real','Ana Claudia','F',465,'Atlas',49.9,'RJ',2009);
-INSERT INTO LIVROS VALUES('19876543219','SQL para leigos','João Nunes','M',450,'Addison',98,'SP',2018);
-INSERT INTO LIVROS VALUES('91234567891','Receitas Caseiras','Celia Tavares','F',210,'Atlas',45.05,'RJ',2008);
-INSERT INTO LIVROS VALUES('59123456789','Pessoas Efetivas','Eduardo Santos','M',390,'Beta',78,'RJ',2018);
-INSERT INTO LIVROS VALUES('31234567890','Habitos Saudáveis','Eduardo Santos','M',630,'Beta',150,'RJ',2019);
-INSERT INTO LIVROS VALUES('49123589071','A Casa Marrom','Hermes Macedo','M',250,'Bubba',60,'MG',2016);
-INSERT INTO LIVROS VALUES('53467123456','Estacio Querido','Geraldo Francisco','M',310,'Insignia',100,'ES',2015);
-INSERT INTO LIVROS VALUES('69128975234','Pra sempre amigas','Leda Silva','F',510,'Insignia',78.16,'ES',2011);
-INSERT INTO LIVROS VALUES('57193495189','Copas Inesqueciveis','Marco Alcantara','M',200,'Larson',130,'RS',2018);
-INSERT INTO LIVROS VALUES('87642345678','O poder da mente','Clara Mafra','F',120,'Continental',56,'SP',2017);
+INSERT INTO livro VALUES('12345678901', 'Cavaleiro Real','Ana Claudia','F',465,'Atlas',2009,'RJ',49.9);
+INSERT INTO livro VALUES('19876543219','SQL para leigos','João Nunes','M',450,'Addison',2018,'SP',98);
+INSERT INTO livro VALUES('91234567891','Receitas Caseiras','Celia Tavares','F',210,'Atlas',2008,'RJ',45.05);
+INSERT INTO livro VALUES('59123456789','Pessoas Efetivas','Eduardo Santos','M',390,'Beta',2018,'RJ',78);
+INSERT INTO livro VALUES('31234567890','Habitos Saudáveis','Eduardo Santos','M',630,'Beta',2019,'RJ',150);
+INSERT INTO livro VALUES('49123589071','A Casa Marrom','Hermes Macedo','M',250,'Bubba',2016,'MG',60);
+INSERT INTO livro VALUES('53467123456','Estacio Querido','Geraldo Francisco','M',310,'Insignia',2015,'ES',100);
+INSERT INTO livro VALUES('69128975234','Pra sempre amigas','Leda Silva','F',510,'Insignia',2011,'ES',78.16);
+INSERT INTO livro VALUES('57193495189','Copas Inesqueciveis','Marco Alcantara','M',200,'Larson',2018,'RS',130);
+INSERT INTO livro VALUES('87642345678','O poder da mente','Clara Mafra','F',120,'Continental',2017,'SP',56);
+
+SELECT * FROM livro;
+
+SELECT 
+    titulo_livro AS 'título',
+    estado_livro AS UF 
+FROM 
+    livro 
+WHERE 
+    autor_sexo = 'M';
+
+SELECT 
+    titulo_livro AS 'título', 
+    editora_livro AS 'editora' 
+FROM 
+    livro 
+WHERE 
+    autor_sexo = 'F' 
+AND 
+    estado_livro = 'SP';
+
+SELECT 
+    titulo_livro AS 'título',
+    editora_livro AS 'editora',
+    ano_publicacao AS 'ano de publicação' 
+FROM 
+    livro
+WHERE 
+    autor_sexo = 'M' 
+AND 
+    estado_livro = 'RJ' 
+OR 
+    estado_livro = 'SP'; 
